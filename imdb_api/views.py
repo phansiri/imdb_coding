0ba1dd48c83c152cde7603dc2b32861e5ef8b705
Lit Phansiri <phansiri@uw.edu>
2019-08-15 21:56:56 +0900

    made dynamic html template and add comments for models

--- imdb_api/views.py
+++ imdb_api/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Movie, Actor, Rating
 
 def movie_list(request):
-    return render(request, 'imdb_api/movie_list.html', {})
+    movies = Movie.objects.all().order_by('name')
+    return render(request, 'imdb_api/movie_list.html', {'movies':movies})
